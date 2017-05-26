936b30177afcf2edc735ea37156fd220a2eb862c
kprog <kprogu@gmail.com>
2017-05-26 22:37:12 +0200

    .

--- app/app.js
+++ app/app.js
@@ -1,8 +1,12 @@
 var express = require('express')
 var app = express()
- 
+
 app.get('/', function (req, res) {
   res.send('Hello World from express.')
 })
- 
+
+app.get('/echo', function (req, res) {
+  res.send('echo.')
+})
+
 app.listen(3000)
